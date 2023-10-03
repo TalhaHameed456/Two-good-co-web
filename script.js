e74fa7a8c8fca3b8293389b98ec93e2e51738c1b
TalhaHameed456 <145604123+TalhaHameed456@users.noreply.github.com>
2023-10-03 07:41:48 -0700

    two-good-co-web

---
+++ script.js
@@ -0,0 +1,144 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform
+        ? "transform"
+        : "fixed",
+    });
+    
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+locomotiveAnimation();
+
+
+function navbarAnimation() {
+    gsap.to("#nav-part1 svg", {
+      transform: "translateY(-100%)",
+      scrollTrigger: {
+        trigger: "#page1",
+        scroller: "#main",
+        start: "top 0",
+        end: "top -5%",
+        scrub: true,
+      },
+    });
+    gsap.to("#nav #twogoodlogo", {
+        transform: "translateY(-132%)",
+        scrollTrigger: {
+          trigger: "#page1",
+          scroller: "#main",
+          start: "top 0",
+          end: "top -5%",
+          scrub: true,
+        },
+      });
+    gsap.to("#nav-part2 #links", {
+      transform: "translateY(-100%)",
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#page1",
+        scroller: "#main",
+        start: "top 0",
+        end: "top -5%",
+        scrub: true,
+      },
+    });
+  }
+  navbarAnimation()
+
+function btnanimation(){   
+var videoanimate = document.querySelector("#video-container");
+var playbtn = document.querySelector("#play");
+
+videoanimate.addEventListener("mouseenter",function(){
+    gsap.to(playbtn,{
+        opacity:1,
+        scale:1,
+    }) 
+})
+videoanimate.addEventListener("mouseleave",function(){
+    gsap.to(playbtn,{
+        opacity:0,
+        scale:0,
+    }) 
+})
+videoanimate.addEventListener("mousemove", function (dets) {
+     var yaxis = dets.clientY - videoanimate.getBoundingClientRect().top;
+
+    gsap.to("#play",{
+        left:dets.clientX-80,
+        top:yaxis-60,
+        
+    })
+  });
+} 
+btnanimation();
+
+
+function page1Animate(){
+    gsap.from("#page1 h1",{
+        y:100,
+        opacity:0,
+        duration:.8,
+        delay:.4,
+        stagger:.3
+    })
+    gsap.from("#page1 #video-container",{
+        scale:.9,
+        delay:1.3,
+        opacity:0,
+        duration:.4,
+        
+        
+    })
+}
+page1Animate();
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+document.querySelectorAll(".child").forEach(function(elem){
+    elem.addEventListener("mouseenter",function(dets){
+        gsap.to("#cursor",{
+            transform: 'translate(-50%,-50%) scale(1)'
+        })
+    })
+    elem.addEventListener("mouseleave",function(dets){
+        gsap.to("#cursor",{
+            transform: 'translate(-50%,-50%) scale(0)'
+        })
+    })
+})
